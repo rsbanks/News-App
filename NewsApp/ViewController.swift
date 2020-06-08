db9a37c09a5ce201a72018ff0f5c5136d36e26da
Rebecca Banks <rsbanks@princeton.edu>
2020-06-08 09:15:33 -0400

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Rebecca Banks on 08/06/2020.
+//  Copyright © 2020 Rebecca Banks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
